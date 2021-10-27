5fde6c13f7ab3c318f3c152a176f5d4edc9154e0
nhelgeson <nhelgeson@wsbeng.com>
2021-10-27 13:07:40 -0500

    updated examples

--- main.js
+++ main.js
@@ -10,20 +10,22 @@ import { Vector as VectorLayer } from 'ol/layer';
 import { GeoJSON } from 'ol/format';
 import { Stroke, Style } from 'ol/style';
 
+import Overlay from 'ol/Overlay';
+
 //////////////////////////////////////////
 // 1. OpenLayers Boilerplate
 //////////////////////////////////////////
 
-const map = new Map({
-  target: 'map',
-  layers: [
-    new TileLayer({
-      source: new OSM()
+const map = new Map({ // create Map object
+  target: 'map', // points to <div> in .html
+  layers: [ // define list of layers
+    new TileLayer({ // type of layer
+      source: new OSM() // source of layer
     })
   ],
-  view: new View({
-    center: fromLonLat([-93.41, 44.92]),
-    zoom: 9
+  view: new View({ // customize map object
+    center: fromLonLat([-93.41, 44.92]), // define center point
+    zoom: 9 // zoom level
   })
 });
 
@@ -31,43 +33,32 @@ const map = new Map({
 
 
 
+//////////////////////////////////////////
+// 2. Add GeoJSON
+//
+// source: https://gisdata.mn.gov/dataset/us-mn-state-metc-trans-regional-trails-land
+//////////////////////////////////////////
 
+// const mnTrails = new VectorLayer({ // new Layer declared as variable
+//   source: new VectorSource ({ // source of the layer
+//     format: new GeoJSON(), // source is geojson
+//     url: '/data/mn-trails.geojson' // location of source
+//   })
+// })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// const map = new Map({
+//   target: 'map',
+//   layers: [
+//     new TileLayer({
+//       source: new OSM()
+//     }),
+//     mnTrails // add layer to map
+//   ],
+//   view: new View({
+//     center: fromLonLat([-93.41, 44.92]),
+//     zoom: 9
+//   })
+// });
 
 
 
@@ -75,13 +66,105 @@ const map = new Map({
 
 
 //////////////////////////////////////////
-// 2. Add an ArcGIS REST feature service
+// 3. Style GeoJSON
 //////////////////////////////////////////
 
-// ArcGIS feature service url with query
-// 
-// query: where 1 = 1 (all records returned)
-//        format = geojson
+// const mnTrails = new VectorLayer({ 
+//   source: new VectorSource ({
+//     format: new GeoJSON(),
+//     url: '/data/mn-trails.geojson'
+//   })
+// })
+
+// mnTrails.setStyle( // dot notation to set style on trails layer
+//   new Style({ // declare new Style properties
+//     stroke: new Stroke({ // stroke = line properties
+//       color: '#5a5a5a',
+//       width: 3,
+//     }),
+//   })
+// )
+
+// const map = new Map({
+//   target: 'map',
+//   layers: [
+//     new TileLayer({
+//       source: new OSM()
+//     }),
+//     mnTrails
+//   ],
+//   view: new View({
+//     center: fromLonLat([-93.41, 44.92]),
+//     zoom: 9
+//   })
+// });
+
+
+
+
+
+//////////////////////////////////////////
+// 4. Add a pop-up
+//////////////////////////////////////////
+
+// const mnTrails = new VectorLayer({
+//   source: new VectorSource({
+//     format: new GeoJSON(),
+//     url: '/data/mn-trails.geojson'
+//   })
+// })
+
+// mnTrails.setStyle(
+//   new Style({
+//     stroke: new Stroke({
+//       color: '#5a5a5a',
+//       width: 4,
+//     }),
+//   })
+// )
+
+// const container = document.getElementById('popup'); // define html content
+
+// const overlay = new Overlay({ 
+//   element: container, // define new overlay html element
+// });
+
+// const map = new Map({
+//   target: 'map',
+//   layers: [
+//     new TileLayer({
+//       source: new OSM()
+//     }),
+//     mnTrails
+//   ],
+//   view: new View({
+//     center: fromLonLat([-93.41, 44.92]),
+//     zoom: 9
+//   }),
+//   overlays: [overlay] // Add Pop-up as overlay to Map
+// });
+
+// map.on('click', function (evt) { // Check for a "click" event
+//   const feature = map.forEachFeatureAtPixel(evt.pixel, function (featureInfo) { // return Feature info based on click location
+//     return featureInfo;
+//   });
+//   if (feature) { // If a feature was clicked on
+//     container.style.display = "block"; // Display html
+//     overlay.setPosition(evt.coordinate); // set overlay position on map
+//     container.innerHTML = feature.get('NAME'); // Display "NAME" property in html
+
+//   } else { // Hide html if feature was not clicked on
+//     container.style.display = "none";
+//   }
+// })
+
+
+
+
+//////////////////////////////////////////
+// (Extra): Add the same data as ArcGIS REST feature service
+// source: https://gisdata.mn.gov/dataset/us-mn-state-metc-trans-regional-trails-land
+//////////////////////////////////////////
 
 // const trailsURL = 'https://arcgis.metc.state.mn.us/server/rest/services/GISLibrary/TrailsRegional/FeatureServer/0/query?where=1%3D1&f=geojson'
 
--- readme.md
+++ readme.md
@@ -5,6 +5,9 @@
     https://slides.com/nihelg/openlayers-101
 
 ## Code
-Inside main.js are the following chunks of commented code:
-    1. OpenLayers Boilerplate
-    2. 
+Inside main.js are the following chunks of commented code:<br>
+    1. OpenLayers Boilerplate<br>
+    2. Add GeoJSON<br>
+    3. Style GeoJSON<br>
+    4. Add a pop-up<br>
+    5. (Advanced) Add ArcGIS REST feature service and style it
